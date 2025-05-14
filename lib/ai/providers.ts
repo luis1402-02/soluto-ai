10b9e7272da60dec3c65a1bd6ee7e3686db0720e
luis1402-02 <marketing@gruposoluto.com.br>
2025-05-13 23:17:09 -0300

    confirmar

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -1,37 +1,38 @@
-import {
-  customProvider,
-  extractReasoningMiddleware,
-  wrapLanguageModel,
-} from 'ai';
-import { xai } from '@ai-sdk/xai';
-import { isTestEnvironment } from '../constants';
-import {
-  artifactModel,
-  chatModel,
-  reasoningModel,
-  titleModel,
-} from './models.test';
+  import {
+    customProvider,
+    extractReasoningMiddleware,
+    wrapLanguageModel,
+  } from 'ai';
+  import { openai } from '@ai-sdk/openai';
+  import { isTestEnvironment } from '../constants';
+  import {
+    artifactModel,
+    chatModel,
+    reasoningModel,
+    titleModel,
+  } from './models.test';
 
-export const myProvider = isTestEnvironment
-  ? customProvider({
-      languageModels: {
-        'chat-model': chatModel,
-        'chat-model-reasoning': reasoningModel,
-        'title-model': titleModel,
-        'artifact-model': artifactModel,
-      },
-    })
-  : customProvider({
-      languageModels: {
-        'chat-model': xai('grok-2-vision-1212'),
-        'chat-model-reasoning': wrapLanguageModel({
-          model: xai('grok-3-mini-beta'),
-          middleware: extractReasoningMiddleware({ tagName: 'think' }),
-        }),
-        'title-model': xai('grok-2-1212'),
-        'artifact-model': xai('grok-2-1212'),
-      },
-      imageModels: {
-        'small-model': xai.image('grok-2-image'),
-      },
-    });
+  export const myProvider = isTestEnvironment
+    ? customProvider({
+        languageModels: {
+          'chat-model': chatModel,
+          'chat-model-reasoning': reasoningModel,
+          'title-model': titleModel,
+          'artifact-model': artifactModel,
+        },
+      })
+    : customProvider({
+        languageModels: {
+          'chat-model': openai('gpt-4o-vision'),
+          'chat-model-reasoning': wrapLanguageModel({
+            model: openai('o4-mini-2025-04-16'),
+            middleware: extractReasoningMiddleware({ tagName: 'think' }),
+          }),
+          'title-model': openai('gpt-4.1-mini-2025-04-14'),
+          'artifact-model': openai('gpt-4.1-2025-04-14'),
+        },
+        imageModels: {
+          'small-model': openai.image('gpt-image-1'),
+        },
+      });
+
